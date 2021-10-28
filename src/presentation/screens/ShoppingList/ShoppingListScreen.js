47a470d62770b71b93d56ee04e2ec0ded55718bf
Vidya Niraula <vidyasagar.niraula@gmail.com>
2021-10-28 10:27:32 -0500

    Modified shopping list screen to accept the new changes

--- src/presentation/screens/ShoppingList/ShoppingListScreen.js
+++ src/presentation/screens/ShoppingList/ShoppingListScreen.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-<<<<<<< HEAD
 import { Text,Touchable,View } from 'react-native';
 import shoppingListStyles from './ShoppingListStyles';
 import {Entypo} from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-=======
-import { Text,View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { kcWhite } from '../../constants/AppColors';
->>>>>>> bca09f1eeb1da0c7e33546b4aac84994bec235d5
 
 
 function ShoppingListScreen(props) {
     return (
-<<<<<<< HEAD
-        <View style = {{flex:1}}>
+        <SafeAreaView style = {{flex:1,backgroundColor:kcWhite, height:"100%"}}>
         <View  style={shoppingListStyles.titleLineStyle}>
 
             <Text style={shoppingListStyles.titleStyle}>Shopping List</Text>
@@ -35,12 +30,7 @@ function ShoppingListScreen(props) {
                 Create a new list to get started!
             </Text> 
             </View>
-        </View>
-=======
-        <SafeAreaView style={{backgroundColor:kcWhite, height:"100%"}}>
-            <Text>Shopping List Screen</Text>
         </SafeAreaView>
->>>>>>> bca09f1eeb1da0c7e33546b4aac84994bec235d5
     )
 }
 
