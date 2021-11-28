b5be94c900201e03a29d913ca28214b8d1e96260
Varuas37 <saurav.panthee@gmail.com>
2021-11-28 17:49:14 -0600

    WIP

--- src/presentation/screens/Inventory/InventoryScreen.js
+++ src/presentation/screens/Inventory/InventoryScreen.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View,Text } from 'react-native'
+import { View, Text } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { kcWhite } from '../../constants/AppColors';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import inventoryScreenStyles from './InventoryScreenStyles';
-import {createStackNavigator, createAppContainer} from '@react-navigation/native-stack';
+import { createStackNavigator, createAppContainer } from '@react-navigation/native-stack';
 import AddGroceryItemScreen from './AddGroceryItemScreen';
+import { connect } from 'react-redux';
 
-function InventoryScreen(props) {
+function InventoryScreen({ navigation }) {
     return (
-        <SafeAreaView style={{flex:1,backgroundColor:kcWhite, height:"100%", alignItems:'center'}}>
-            
-            <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: kcWhite, height: "100%", alignItems: 'center' }}>
+
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <TouchableOpacity
-                style={inventoryScreenStyles.inventoryButtonStyle}
-                onPress={()=>props.navigation.navigate('Add Grocery Items')}>
+                    style={inventoryScreenStyles.inventoryButtonStyle}
+                    onPress={() => navigation.navigate('Add Grocery Items')}>
                     <Text style={inventoryScreenStyles.inventoryButtonText}>Add items to Inventory</Text>
-                    </TouchableOpacity>
+                </TouchableOpacity>
             </View>
-       </SafeAreaView>
+        </SafeAreaView>
     )
 }
 
@@ -27,4 +28,10 @@ InventoryScreen.propTypes = {
 
 }
 
-export default InventoryScreen;
+
+const mapStateToProps = (state) => ({
+    inventoryList: state.inventoryList,
+});
+
+export default connect(mapStateToProps, {})(InventoryScreen);
+
