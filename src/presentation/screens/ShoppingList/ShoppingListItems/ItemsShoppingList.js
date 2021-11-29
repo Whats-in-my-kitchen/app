e0f28df9b3fbc8e04ed4f3cba5b4eb0b9de167b1
Blake Robinson <brobs0111@gmail.com>
2021-11-28 21:26:39 -0600

    shopping list items added with checkboxes

--- src/presentation/screens/ShoppingList/ShoppingListItems/ItemsShoppingList.js
+++ src/presentation/screens/ShoppingList/ShoppingListItems/ItemsShoppingList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text } from 'react-native'
+import { ListItem, Avatar, Icon, Image } from "react-native-elements";
 import { kcWhite, kcLightGrey, kcDarkGrey } from '../../../constants/AppColors';
 function ItemsShoppingList({ route }) {
     const { item } = route.params;
@@ -9,10 +10,23 @@ function ItemsShoppingList({ route }) {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: kcWhite, height: "100%", flexDirection: 'column' }}>
             <View>
-                <Text>
-                    {console.log('📝 📝' + item.groceryItem)}
-                    {JSON.stringify(item.groceryItem)}
-                </Text>
+            { 
+                    item.groceryItem.map((items, i) => (
+                    
+                    <ListItem key={i}>
+                    <Avatar
+                    square
+                    overlayContainerStyle={{backgroundColor: kcDarkGrey}}
+                    onPress={() => console.log("pressed")}
+                    title={items.name[0]} />
+                    <ListItem.Content>
+                    <ListItem.Title>{items.name}</ListItem.Title>
+                    <ListItem.Subtitle>{items.count}</ListItem.Subtitle>
+                    </ListItem.Content>
+                    <ListItem.CheckBox/>
+                </ListItem>
+                    ))
+                    }
             </View>
         </SafeAreaView>
     )
